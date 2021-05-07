f074a237d535bc1ad4f531cd04c1eb05f2e86690
aytug.sevgi <aytug3667@gmail.com>
2021-05-07 14:22:52 +0300

    fix accessability

--- ps5Controller/ps5Controller/HomeModule/View/HomeCollectionViewCell.swift
+++ ps5Controller/ps5Controller/HomeModule/View/HomeCollectionViewCell.swift
@@ -2,8 +2,8 @@ import UIKit
 
 final class HomeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak private var cellButton: UIButton!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var subtitleLabel: UILabel!
     
     func configure(with homeViewModel: HomeViewModel) {
         let image = UIImage(named: homeViewModel.image)
